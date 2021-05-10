9bbebd07f009020dcc720fbba90bb5ae3e859f65
Tofan Petru <tofanpetru12@gmail.com>
2021-05-10 16:09:59 +0300

    Added BindJson

---
+++ BindJson/AppSettings.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace BindJson
+{
+    public class AppSettings
+    {
+        private static AppSettings _instance;
+
+        public static AppSettings Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+
+                    IConfiguration config = new ConfigurationBuilder()
+                       .AddJsonFile("appSettings.json", true, true)
+                       .Build();
+                    _instance = new AppSettings();
+
+                    config.Bind(_instance);
+
+                }
+
+                return _instance;
+            }
+        }
+
+        public string ConnectionString { get; set; }
+
+        public string Path { get; set; }
+    }
+}
---
+++ BindJson/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+
+using System;
+
+namespace BindJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = AppSettings.Instance.ConnectionString;
+
+            Console.WriteLine(connectionString);
+            Console.Read();
+        }
+    }
+}
